def5f04cee577176abf87185f7f97048750d5555
Wingrez <31106425+wingrez@users.noreply.github.com>
2019-06-28 13:49:15 +0800

    增加图片切换悬浮窗
    增加移动悬浮窗事件

--- app/src/main/java/com/wingrez/lovelypet/FWService.java
+++ app/src/main/java/com/wingrez/lovelypet/FWService.java
@@ -2,10 +2,64 @@ package com.wingrez.lovelypet;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
-import android.widget.Toast;
+import android.os.Message;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class FWService extends Service {
+
+    public static boolean isFWRunning = false;
+
+    private WindowManager windowManager;
+    private WindowManager.LayoutParams layoutParams;
+
+    private View displayView;
+
+    private int[] images;
+    private int imageIndex = 0;
+
+    private Handler changeImageHandler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        isFWRunning = true;
+
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        layoutParams = new WindowManager.LayoutParams();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { //系统版本号大于等于8.0
+            layoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else { //系统版本号小于8.0
+            layoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+
+        layoutParams.format = PixelFormat.RGBA_8888;
+        layoutParams.gravity = Gravity.CENTER;
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        layoutParams.height = 100;
+        layoutParams.width = 100;
+        layoutParams.x = 0; //使悬浮窗处于水平和垂直居中位置
+        layoutParams.y = 0;
+
+        images = new int[]{
+                R.drawable.image_1,
+                R.drawable.image_2,
+                R.drawable.image_3
+        };
+
+        changeImageHandler = new Handler(this.getMainLooper(), changeImageCallback);
+
+    }
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -14,14 +68,83 @@ public class FWService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // Let it continue running until it is stopped.
-        Toast.makeText(this, "服务已经启动", Toast.LENGTH_LONG).show();
+        showFW();
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "服务已经停止", Toast.LENGTH_LONG).show();
+        windowManager.removeView(displayView);
+        isFWRunning = false;
     }
+
+    /**
+     * 显示悬浮窗
+     */
+    private void showFW() {
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        displayView = layoutInflater.inflate(R.layout.image_display, null);
+        displayView.setOnTouchListener(new FloatingOnTouchListener());
+        ImageView imageView = displayView.findViewById(R.id.image_display_imageview);
+        imageView.setImageResource(images[imageIndex]);
+        windowManager.addView(displayView, layoutParams);
+
+        changeImageHandler.sendEmptyMessageDelayed(0, 100);
+    }
+
+    /**
+     * 切换图片的回调方法
+     */
+    private Handler.Callback changeImageCallback = new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            if (msg.what == 0) {
+                imageIndex++;
+                if (imageIndex >= images.length) {
+                    imageIndex = 0;
+                }
+                if (displayView != null) {
+                    ((ImageView) displayView.findViewById(R.id.image_display_imageview)).setImageResource(images[imageIndex]);
+                }
+
+                changeImageHandler.sendEmptyMessageDelayed(0, 100);
+            }
+            return false;
+        }
+    };
+
+    /**
+     * 悬浮窗移动事件
+     */
+    private class FloatingOnTouchListener implements View.OnTouchListener {
+        private float x;
+        private float y;
+
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    x = event.getRawX();
+                    y = event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    float nowX = event.getRawX();
+                    float nowY = event.getRawY();
+                    float movedX = nowX - x;
+                    float movedY = nowY - y;
+                    x = nowX;
+                    y = nowY;
+                    layoutParams.x = layoutParams.x + Math.round(movedX);
+                    layoutParams.y = layoutParams.y + Math.round(movedY);
+                    windowManager.updateViewLayout(view, layoutParams);
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+    }
+
+
 }
--- app/src/main/java/com/wingrez/lovelypet/MainActivity.java
+++ app/src/main/java/com/wingrez/lovelypet/MainActivity.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -20,18 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_openFW= this.<Button>findViewById(R.id.btn_openFW);
+        btn_openFW = this.<Button>findViewById(R.id.btn_openFW);
     }
 
     /**
      * Activity返回结果
+     *
      * @param requestCode
      * @param resultCode
      * @param data
      */
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 0) {
             if (!Settings.canDrawOverlays(this)) {
                 Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
@@ -44,36 +44,51 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * 按钮点击事件
+     *
      * @param view
      */
-    public void onClick (View view){
-        switch (view.getId()){
-            case R.id.btn_openFW :
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_openFW: //开启悬浮窗
                 startFWService(view);
                 break;
-            case R.id.btn_closeFW:
+            case R.id.btn_closeFW: //关闭悬浮窗
                 stopFWService(view);
+                break;
         }
     }
 
-    private void startFWService(View view){
+    /**
+     * 开启悬浮窗服务
+     *
+     * @param view
+     */
+    private void startFWService(View view) {
+        if (FWService.isFWRunning == true) { //检查悬浮窗开启状态，只允许开启一个悬浮窗
+            Toast.makeText(this, "悬浮窗已开启", Toast.LENGTH_SHORT);
+            return;
+        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { //需要判断系统版本是否大于Android 6.0
-            if (!Settings.canDrawOverlays(this)) { //因为版本号大于6.0的系统才可以调用此方法
+            if (!Settings.canDrawOverlays(this)) { //因为版本号大于6.0的系统才可以调用此方法，未授权状态
                 Toast.makeText(this, "未授权开启悬浮窗", Toast.LENGTH_SHORT);
-                startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), 0);
-            } else {
+                startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), 0); //请求授权
+            } else { //已授权状态
+                Toast.makeText(this, "悬浮窗已开启", Toast.LENGTH_SHORT);
                 startService(new Intent(MainActivity.this, FWService.class));
             }
-        } else {
+        } else { //版本号小于6.0，直接开启悬浮窗，无需授权
+            Toast.makeText(this, "悬浮窗已开启", Toast.LENGTH_SHORT);
             startService(new Intent(MainActivity.this, FWService.class));
         }
     }
 
     /**
-     * 关闭悬浮窗口
+     * 关闭悬浮窗服务
+     *
      * @param view
      */
-    private void stopFWService(View view){
+    private void stopFWService(View view) {
         stopService(new Intent(MainActivity.this, FWService.class));
     }
 
