db8f62bbde1463661e990c7d9c7a46b610349df1
Wingrez <31106425+wingrez@users.noreply.github.com>
2019-07-02 02:32:20 +0800

    增加注释，修改代码形式

--- app/src/main/java/com/wingrez/lovelypet/receiver/BroadcastReceiver.java
+++ app/src/main/java/com/wingrez/lovelypet/receiver/BroadcastReceiver.java
@@ -5,15 +5,22 @@ import android.content.Intent;
 
 import com.wingrez.lovelypet.service.FWService;
 
+/**
+ * 广播接收类
+ */
 public class BroadcastReceiver extends android.content.BroadcastReceiver {
     private static final String ACTION_BOOT = "android.intent.action.BOOT_COMPLETED";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (ACTION_BOOT.equals(intent.getAction())) {
-            Intent FWIntent = new Intent(context, FWService.class);
-            FWIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startService(FWIntent);
+        switch (intent.getAction()) {
+            case ACTION_BOOT: //开机启动
+                Intent FWIntent = new Intent(context, FWService.class);
+                FWIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startService(FWIntent);
+                break;
+            default:
+                break;
         }
     }
 }
